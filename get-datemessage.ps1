a2f9de97c18fec0d480b683a935b17f2aaa14cde
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-05 21:38:08 -0500

    Initial commit; created first script with description inside

---
+++ get-datemessage.ps1
@@ -0,0 +1,6 @@
+# A basic PowerShell script to print a welcome message and the date and time
+# at the top of the PowerShell CLI prompt.
+# Created: 12/05/2021 - JHD
+#
+# cmdlet name: get-datemessage.ps1
+#
