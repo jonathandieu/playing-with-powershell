e8560314264f300e196c0ae811d349ebe8ffa8e0
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-08 00:18:33 -0500

    Adding system processor implementation

--- get-specs.ps1
+++ get-specs.ps1
@@ -3,3 +3,6 @@ Write-Host "Welcome to the script of fetching computer Information"
 Write-host "The BIOS Details are as follows"
 Get-CimInstance -ClassName Win32_BIOS
 
+# Get system processor
+Write-Host "The system's processor is: "
+Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
