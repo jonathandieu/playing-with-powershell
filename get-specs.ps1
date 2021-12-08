e11be27caf02ef1535bc2b988a1af6da4f8d7c4a
Jonathan Dieu <jonathanhuydieu@gmail.com>
2021-12-08 00:19:37 -0500

    adding computer manufacturer and physical memory

--- get-specs.ps1
+++ get-specs.ps1
@@ -6,3 +6,7 @@ Get-CimInstance -ClassName Win32_BIOS
 # Get system processor
 Write-Host "The system's processor is: "
 Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
+
+# Get computer Manufacture and Physical Memory
+Write-Host "The computer Manufacturer and Physical Memory Details are as follows:"
+Get-CimInstance -ClassName Win32_QuickFixEngineering
